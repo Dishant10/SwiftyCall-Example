1d9ce19620d19f27e63938ac3debd5674962329b
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-06-04 15:21:13 +0530

    Initial Commit

---
+++ GenericNetworking/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GenericNetworking
+//
+//  Created by Dishant Nagpal on 04/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenericNetworking/GenericNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericNetworkingApp.swift
+//  GenericNetworking
+//
+//  Created by Dishant Nagpal on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GenericNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
